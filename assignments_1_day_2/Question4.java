ce659da3d1eb1ef3dc274516c33885927aa1050f
uguney <80786342+uguney@users.noreply.github.com>
2021-03-18 17:43:31 +0300

    Add files via upload

---
+++ assignments_1_day_2/Question4.java
@@ -0,0 +1,11 @@
+package java_assignments.assignments_1_day_2;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Q");
+		
+	}
+
+}
---
+++ assignments_1_day_2/Question5.java
@@ -0,0 +1,18 @@
+package java_assignments.assignments_1_day_2;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("* * * * * * *");
+		System.out.println("* *       * *");
+		System.out.println("*   *   *   *");
+		System.out.println("*     *     *");
+		System.out.println("*   *   *   *");
+		System.out.println("* *       * *");
+		System.out.println("* * * * * * *");
+		
+	}
+
+}
