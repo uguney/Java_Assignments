2c78f85f2d46bda8c8fe7f43e429c1ffdd3358a6
uguney <80786342+uguney@users.noreply.github.com>
2021-03-18 17:54:42 +0300

    Add files via upload

---
+++ assignments_1_day_2/Question4.java
@@ -0,0 +1,17 @@
+package java_assignments.assignments_1_day_2;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Q");
+		
+		/* NOTE:
+		 * there were some mistakes about placing
+		 * the main method, braces, class header etc.
+		 * and I rearranged them.*/
+		
+	}
+
+}
---
+++ assignments_1_day_2/Question6.java
@@ -0,0 +1,24 @@
+package java_assignments.assignments_1_day_2;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("   ^ ");
+		System.out.println("  / \\ ");
+		System.out.println(" /   \\ ");
+		System.out.println("/     \\ ");
+		System.out.println("|     | ");
+		System.out.println("|     | ");
+		System.out.println("|     | ");
+		System.out.println("|     | ");
+		System.out.println("|     | ");
+		System.out.println("|     | ");
+		System.out.println("|     | ");
+		System.out.println("------- ");
+	}
+
+}
